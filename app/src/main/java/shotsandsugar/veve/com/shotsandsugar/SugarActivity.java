dfd6fb6b4083806b8c4db52b98f9dc920aa82415
oudun <oudun@mail.ru>
2018-10-05 19:11:10 +0300

    Sugar Activity

--- app/src/main/java/shotsandsugar/veve/com/shotsandsugar/SugarActivity.java
+++ app/src/main/java/shotsandsugar/veve/com/shotsandsugar/SugarActivity.java
@@ -42,52 +42,6 @@ public class SugarActivity extends AppCompatActivity {
             }
         });
 
-        final GridLayout gridLayout = (GridLayout) findViewById(R.id.gridLayout);
-        gridLayout.setColumnCount(COLUMNS_NUM);
-        gridLayout.setPadding(PADDING, PADDING, PADDING, PADDING);
-
-        gridLayout.addOnLayoutChangeListener(new View.OnLayoutChangeListener() {
-            @Override
-            public void onLayoutChange(View view, int i0, int i1, int i2, int i3, int i4, int i5, int i6, int i7) {
-
-                gridLayout.setBackgroundColor(Color.GREEN);
-
-            }
-        });
-
-//        int width = 240; //gridLayout.getWidth();
-//        int height = 260; //gridLayout.getHeight();
-
-        int width = gridLayout.getWidth();
-        int height = gridLayout.getHeight();
-
-        for (int i=0; i < MAX_SUGAR_LEVEL; i++) {
-
-            Button button = new Button(getApplicationContext());
-            button.setBackgroundColor(Color.GRAY);
-            button.setTextColor(Color.WHITE);
-            Log.d(getClass().getName(), "Grid width is " + width);
-            Log.d(getClass().getName(), "Button width is " + (width - PADDING * (COLUMNS_NUM + 1))/ COLUMNS_NUM);
-            int buttonWidth = (width - PADDING * (COLUMNS_NUM + 1))/ COLUMNS_NUM;
-            int buttonHeight = (height - PADDING * (ROWS_NUM + 1))/ ROWS_NUM;
-            button.setWidth(buttonWidth);
-            button.setHeight(buttonHeight);
-
-            button.setText(String.valueOf(i));
-
-            ViewGroup.MarginLayoutParams marginParams = new ViewGroup.MarginLayoutParams(buttonWidth, buttonHeight);
-            marginParams.setMargins(PADDING/2, PADDING/2, PADDING/2, PADDING/2);
-            button.setLayoutParams(marginParams);
-
-            Log.d(getClass().getName(), String.format(
-                    "Adding button #%d with size %dx%d labelled %s", i,
-                    button.getWidth(), button.getHeight(), button.getText()));
-
-            gridLayout.addView(button);
-
-        }
-
-
     }
 
 }
