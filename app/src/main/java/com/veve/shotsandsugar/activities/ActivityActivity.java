6d40358afe639afe9e4bc23c1d07ed4679905cff
oudun <oudun@mail.ru>
2018-11-08 19:36:27 +0300

    Time picker added

--- app/src/main/java/com/veve/shotsandsugar/activities/ActivityActivity.java
+++ app/src/main/java/com/veve/shotsandsugar/activities/ActivityActivity.java
@@ -9,10 +9,12 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.EditText;
+import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.SpinnerAdapter;
 import android.widget.TextView;
@@ -22,13 +24,21 @@ import com.veve.shotsandsugar.Constants;
 import com.veve.shotsandsugar.R;
 import com.veve.shotsandsugar.model.Activity;
 
+import java.util.Calendar;
 import java.util.List;
+import java.util.Locale;
 
 public class ActivityActivity extends DatabaseActivity {
+
+    final static Calendar CALENDAR = Calendar.getInstance();
 
     static List<Activity> activityList;
 
     static TimePickerDialog timePickerDialog;
+
+    EditText fromTime;
+
+    EditText toTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,6 +57,9 @@ public class ActivityActivity extends DatabaseActivity {
             }
         });
 
+        RadioGroup intervalGroup = findViewById(R.id.interval);
+        intervalGroup.check(0);
+
 
         try {
             activityList = new GetActivitiesTask().execute().get();
@@ -57,18 +70,42 @@ public class ActivityActivity extends DatabaseActivity {
         Spinner spinner = findViewById(R.id.activities);
         spinner.setAdapter(new ActivitiesAdapter(getApplicationContext()));
 
+        fromTime = findViewById(R.id.fromTime);
+
+        toTime = findViewById(R.id.toTime);
+
     }
 
-    EditText fromTime = findViewById(R.id.fromTime);
+
+
 
     public void setFrom(View view) {
+        int hour = CALENDAR.get(Calendar.HOUR_OF_DAY);
+        int minute = CALENDAR.get(Calendar.MINUTE);
         timePickerDialog = new TimePickerDialog(this, new TimePickerDialog.OnTimeSetListener() {
             @Override
             public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
-                fromTime.setText(String.format("%d:%d", hourOfDay, minute));
+                fromTime.setText(String.format(Locale.getDefault(), "%d2:%d2", hourOfDay, minute));
+                Log.i(getClass().getName(), String.format("%d2:%d2", hourOfDay, minute));
             }
-        }, 1, 1, false );
+        }, hour, minute, true );
         timePickerDialog.show();
+    }
+
+    public void setTo(View view) {
+        int hour = CALENDAR.get(Calendar.HOUR_OF_DAY);
+        int minute = CALENDAR.get(Calendar.MINUTE);
+        timePickerDialog = new TimePickerDialog(this, new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                toTime.setText(String.format(Locale.getDefault(), "%2d:%2d", hourOfDay, minute));
+                Log.i(getClass().getName(), String.format("%dd:%dd", hourOfDay, minute));
+            }
+        }, hour, minute, true );
+        timePickerDialog.show();
+    }
+
+    public void checkOthers(View view) {
     }
 
     static class GetActivitiesTask extends AsyncTask<Void, Void, List<Activity>> {
@@ -84,7 +121,7 @@ public class ActivityActivity extends DatabaseActivity {
 
         Context context;
 
-        public ActivitiesAdapter(Context context) {
+        ActivitiesAdapter(Context context) {
             this.context = context;
         }
 
@@ -114,6 +151,7 @@ public class ActivityActivity extends DatabaseActivity {
                 view.setTextSize(16);
                 view.setTextColor(Color.BLACK);
                 view.setBackgroundResource(R.drawable.drop_down);
+                view.setGravity(Gravity.CENTER|Gravity.START);
                 view.setWidth(300);
                 convertView = view;
             }
